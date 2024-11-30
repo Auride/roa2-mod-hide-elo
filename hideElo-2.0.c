375794ab4b91e31ffd6005cb06ef4ad8da36bb80
auride <themuffinelf@gmail.com>
2024-11-30 16:28:00 -0500

    Correcting and expanding comments, reformatting some code in hideElo-2.0.c

--- hideElo-2.0.c
+++ hideElo-2.0.c
@@ -51,14 +51,13 @@ MODULEENTRY32 FindModule(DWORD ProcessId, LPCSTR ModuleName) {
 	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, ProcessId);
 	MODULEENTRY32 me32;
 	me32.dwSize = sizeof(MODULEENTRY32);
-	if (!Module32First(snapshot, &me32)) me32.modBaseAddr = NULL;
-	else {
-		while (TRUE) {
-			if (strcmp(ModuleName, me32.szModule) == 0) break;
-			if (!Module32Next(snapshot, &me32)) {
-				me32.modBaseAddr = NULL;
-				break;
-			}
+	BOOL anyModules = Module32First(snapshot, &me32);
+	if (!anyModules) me32.modBaseAddr = NULL;
+	while (anyModules) {
+		if (strcmp(ModuleName, me32.szModule) == 0) break;
+		if (!Module32Next(snapshot, &me32)) {
+			me32.modBaseAddr = NULL;
+			break;
 		}
 	}
 	CloseHandle(snapshot);
@@ -100,22 +99,19 @@ VOID WriteIntoMemory(HANDLE Process, DWORD64 Base, DWORD64 Size, PVOID Buffer)
  */
 byte hexCharToInt(char hex) {
 	byte nibble = hex - '0';
-	if (nibble > 9) {
-		nibble = hex - 'a';
-		if (nibble > 5) {
-			nibble = hex - 'A';
-			if (nibble > 5) return 0xFF;
-		}
-		nibble += 10;
-	}
-	return nibble;
+	if (nibble <= 9) return nibble;
+	nibble = hex - 'a';
+	if (nibble <= 5) return nibble + 10;
+	nibble = hex - 'A';
+	if (nibble <= 5) return nibble + 10;
+	return 0xFF;
 }
 
 /**
  * Represents an array of bytes to scan memory for, where some of those bytes
  * are treated as wildcards (any byte in memory will match). If the byte in
- * .bytes is checked, the corresponding byte in .mask is TRUE. If it's a
- * wildcard, the .mask byte is FALSE.
+ * .bytes is checked, the corresponding byte in .mask is 0xFF. If it's a
+ * wildcard, the .mask byte is 0x00.
  */
 struct aobPattern {
 	size_t len;
@@ -136,8 +132,9 @@ void freeAOBPattern(struct aobPattern a) {
  * Takes in an IDA-style AOB pattern and returns an equivalent aobPattern
  * struct. IDA style comes from the IDA disassembler, where an array-of-bytes
  * pattern can be represented by a string of hex bytes separated by spaces,
- * where wildcards are represented by ??, e.g. "7C 37 ?? 81".This is the same
- * style used by Cheat Engine.
+ * where wildcards are represented by ??, e.g. "7C 37 ?? 81". This is the same
+ * style used by Cheat Engine, except half-byte wildcards (e.g. ?7 or 3?) are
+ * not supported (because I didn't see the benefit).
  *
  * If the input is not a valid pattern, the returned aobPattern will have a .len
  * of 0, and .bytes and .mask will be NULL.
@@ -166,7 +163,7 @@ struct aobPattern idaPatternToStruct(char* idaPattern) {
 		
 		if (char1 == '?' && char2 == '?') {
 			ret.bytes[a] = '\x00';
-			ret.mask[a] = FALSE;
+			ret.mask[a] = 0x00;
 			continue;
 		}
 		byte nibble1 = hexCharToInt(char1);
@@ -176,7 +173,7 @@ struct aobPattern idaPatternToStruct(char* idaPattern) {
 			break;
 		}
 		ret.bytes[a] = (nibble1 << 4) + nibble2;
-		ret.mask[a] = TRUE;
+		ret.mask[a] = 0xFF;
 	}
 	if (!valid) {
 		ret.len = 0;
@@ -187,19 +184,56 @@ struct aobPattern idaPatternToStruct(char* idaPattern) {
 	return ret;
 }
 
-/**
- * 
- */
 #define AOB_NO_MATCH -1
 #define AOB_PATTERN_INVALID -2
+/**
+ * Searches the memory of the given process between the addresses moduleBase and
+ * moduleEnd for a segment matching the given pattern (provided in IDA format,
+ * except only full-byte wildcards ?? are supported), then writes the bytes in
+ * patchBytes at the address of the match + patchOffset. 
+ *
+ * This is meant to be easy to adapt from a CheatEngine AOB Injection script.
+ * Use `FindProcessId` and `OpenProcess` to get the `process` handle, and
+ * `FindModule` to find the boundaries of the module based on the name you find
+ * in the Cheat Engine AOB script call to `aobscanmodule()`. In the same call,
+ * you'll see the AOB sequence of bytes, which can be copied into a C string
+ * as-is for the `idaPattern` parameter.
+ *
+ * For `patchOffset`, find the number following the injection-point label (the
+ * same as the first parameter of `aobscanmodule`) which you should see once
+ * after [ENABLE] and once after [DISABLE], e.g. `INJECT_MyCheat+0B:` would
+ * correspond to a patchOffset of 0x0B. Note that the offset is always in hex,
+ * so you should use a 0x hex literal in this program. 
+ *
+ * Finally, for `patchBytes`, you will need to see the sequence of bytes
+ * corresponding to the assembled machine code in the [ENABLE] section. You can
+ * get this either by using the `Disassemble this code region` option and
+ * copying the bytes of the injected code, or by using any other x86 assembler
+ * software.
+ *
+ * If, after searching the entire memory region, no match is found, no changes
+ * are made and `AOB_NO_MATCH` is returned. If the given pattern is not valid,
+ * `AOB_PATTERN_INVALID` is returned. The function will also printf a
+ * corresponding message (including the `description` parameter) when it
+ * returns, both for success and failure.
+ *
+ * !!!IMPORTANT NOTE!!!: This does NOT create jumps and code caves! It only
+ * write bytes directly to the target location. Thus, you must make sure your
+ * cheat is a simple byte substitution of the exact same length as the
+ * instructions you are trying to overwrite. Cheat engine will create jumps and
+ * code caves by default if you use the AOB Injection template, even if your new
+ * code would fit within the space of the target, so you'll need to figure this
+ * out manually. This will usually require some extra creativity and some NOPs,
+ * but some cheats may simply not be possible with this limitation.
+ */
 DWORD64 aobInject(
 	const char* description,
 	const HANDLE process, const DWORD64 moduleBase, const DWORD64 moduleEnd,
 	char* idaPattern, const DWORD64 patchOffset,
-	byte* patchBytes
+	char* patchBytes
 ) {
 	struct aobPattern aob = idaPatternToStruct(idaPattern);
-	if (aob.bytes == NULL) {
+	if (aob.len == 0) {
 		printf("ERROR: \"%s\" failed because the AOB pattern \"%s\" is invalid.\n", description, idaPattern);
 		return AOB_PATTERN_INVALID;
 	}
@@ -207,11 +241,11 @@ DWORD64 aobInject(
 	DWORD64 matchAddr;
 	size_t aobIndex = 0;
 	DWORD64 scanAddr = moduleBase;
-	DWORD64 lastScanAddr = 0;
+	DWORD64 lastScanAddr = moduleBase;
 	
 	// Memory buffer size, must be no more than 0x1000 or this will cause the
 	// target process to crash (not 100% sure why...)
-	#define MBSIZE 0x1000
+	#define MBSIZE 0x1000ULL
 	char memBuffer[MBSIZE];
 	ReadFromMemory(process, scanAddr, MBSIZE, memBuffer);
 	size_t buffIndex = 0;
@@ -220,9 +254,7 @@ DWORD64 aobInject(
 	while (TRUE) {
 		if (!aob.mask[aobIndex] || memBuffer[buffIndex] == aob.bytes[aobIndex]) {
 			// If finding the start of a new match, save address
-			if (aobIndex == 0) {
-				matchAddr = scanAddr + buffIndex;
-			}
+			if (aobIndex == 0) matchAddr = scanAddr + buffIndex;
 			++aobIndex;
 			if (aobIndex == aob.len) break;
 		}
@@ -230,8 +262,8 @@ DWORD64 aobInject(
 			// Return match head to start of search string
 			aobIndex = 0;
 			// Return mem buffer segment to contain byte after start of previous
-			// match. If MBSIZE is a power of 2, this will be optimized to an
-			// AND instruction.
+			// match. If MBSIZE is a power of 2, `/MBSIZE*MBSIZE` should be
+			// optimized to `&-MBSIZE`.
 			scanAddr = (matchAddr+1)/MBSIZE*MBSIZE; 
 			// Return mem buffer head to 1 byte after start of previous match (1
 			// will be added by next line)
@@ -260,9 +292,9 @@ DWORD64 aobInject(
 	DWORD64 patchLen = strlen(patchBytes);
 	
 	DWORD64 patchAddr = matchAddr + patchOffset;
-	WriteIntoMemory(process, matchAddr + patchOffset, patchLen, patchBytes);
-	printf("\"%s\" patch applied at offset 0x%08X.\n", description, patchAddr);
-	return matchAddr;
+	WriteIntoMemory(process, patchAddr, patchLen, patchBytes);
+	printf("\"%s\" patch applied at address 0x%08llX (offset 0x%08llX).\n", description, patchAddr, patchAddr-moduleBase);
+	return patchAddr;
 }
 
 #define TARGET_PROCESS_NAME "Rivals2-Win64-Shipping.exe"
@@ -287,21 +319,18 @@ int main() {
 	}
 	const DWORD64 modEnd = modBase + moduleData.modBaseSize;
 	HANDLE process = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-
-	// In order to guarantee that the master Elo tier icon is always shown
-	// (normally requires >=1500 Elo), we can just overwrite the place where the
-	// game reads your actual Elo with an immediate constant of decimal 9999.
+	
 	// The check for the icon displayed when you press the "RANKED" button from
 	// the "ONLINE" menu is, for some reason, separate from the other checks.
 	// The function being edited normally returns a number between 1 and 7
 	// (inclusive) to determine the rank tier icon shown, where 1 is stone and 7
-	// is master. If 0 is returned, then the icons come up blank/missing/empty.
-	// The function normally has 7 branches where it compares against an array
-	// of tier cutoffs (500, 700, 900, 1100, 1300, and >=1500 for master) but by
-	// nopping out the first comparison and changing the first return to 0, we
-	// can force this outcome! By overwriting this function rather than just
-	// setting the Elo in memory directly, we guarantee it will always work and
-	// avoid any chance of accidentally sending the wrong Elo to the servers.
+	// is master. The function normally has 7 branches where it compares against
+	// an array of tier cutoffs (500, 700, 900, 1100, 1300, and >=1500 for
+	// master) but by nopping out the first comparison and changing the first
+	// return to 7, we can force this outcome! By overwriting this function
+	// rather than just setting the Elo in memory directly, we guarantee it will
+	// always work and avoid any chance of accidentally sending the wrong Elo to
+	// the servers.
 	aobInject(
 		"Force Ranked search tier icon to be Master",
 		process, modBase, modEnd,
@@ -335,7 +364,7 @@ int main() {
 		/* nop (5 bytes) */"\x0F\x1F\x44\x00\x01"
 	);
 
-	// This hides your previous and new Elo displayed after a ranked se
+	// This hides your previous and new Elo displayed after a ranked set.
 	aobInject(
 		"Hide post-set Elo display",
 		process, modBase, modEnd,
